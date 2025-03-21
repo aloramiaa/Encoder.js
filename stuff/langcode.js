d31ea4230226535d82c7397d926b6f06ab7f795c
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-03-22 00:50:13 +0500

    Update langcode.js

--- stuff/langcode.js
+++ stuff/langcode.js
@@ -1,3 +1,6 @@
 export const langcode = (id) => {
     return id == 1 ? 'ru' : id == 2 ? 'fr' : id == 3 ? 'ua' : 'en';
 }
+export const debug = (path) => {
+    return path.replace(/\/(.*?)\/(.*?)/, '/$2');
+}
