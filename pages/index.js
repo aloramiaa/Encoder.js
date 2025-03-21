be4605e0e702f98c0b3860d0e40d6fa4c0985c5c
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-03-22 00:50:11 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ import { useTranslations } from 'next-intl'
 
 import { useGlobalContext } from '@/global-context'
 
-import { langcode } from '@/stuff/langcode';
+import { debug, langcode } from '@/stuff/langcode';
 
 const Home = (props) => {
   const { locale, locales } = useGlobalContext()
@@ -90,9 +90,9 @@ const Home = (props) => {
           >
             <div>
               {locales.map((all_languages, index) => (
-                <Link href={`/${langcode(index).replace('en','')}`}>
+                <Link href={debug(`/${langcode(index).replace('en','')}`)}>
                   <Fragment key={index}>
-                    <Link href={`/${langcode(index).replace('en','')}`}>
+                    <Link href={debug(`/${langcode(index).replace('en','')}`)}>
                       <div className="home-language-switcher-item">
                         <span className="home-language-link">
                           {all_languages?.name}
