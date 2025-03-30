24c1dcc68bb30fa0e9f45283693b80e9ec0df50e
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-03-30 17:30:41 +0500

    fix

--- _just/dangerously-insert-files/encoder.js@1.0.1.js
+++ _just/dangerously-insert-files/encoder.js@1.0.1.js
@@ -756,10 +756,17 @@ function encode5 (text, key, compress) {
   if (compress) {
     const doubleEncoded = encode4(encoded_, key, compress);
     const numEncoded = numEncode(text);
+    const customDecode5 = (text) => {
+      try {
+        return decode5(text);
+      } catch {
+        return false;
+      }
+    }
     if (doubleEncoded.length < encoded_.length) {
       output_ = `J${doubleEncoded}`;
     }
-    if (/^[0-9]+$/.test(text) && (key === undefined || key === null || key === '') && decode5(numEncoded) === text) {output_ = numEncoded}
+    if (/^[0-9]+$/.test(text) && (key === undefined || key === null || key === '') && customDecode5(numEncoded) === text) {output_ = numEncoded}
   }
   if (even_or_odd(output_.length)) output_ = output_.split('').reverse().join('');
   return output_;
