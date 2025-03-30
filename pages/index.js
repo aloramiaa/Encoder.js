127650640b390ba35819e4df781798abdf2b92d1
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-03-31 01:00:58 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -88,9 +88,9 @@ const Home = (props) => {
           >
             <div>
               {locales.map((all_languages, index) => (
-                <Link href={`${process.env.URLprefix}${locales[index].short}`}>
+                <Link href={`${process.env.URLprefix}${locales[index].short}`.replace(/\/(.*?)\/(.*?)/, '/$2')}>
                   <Fragment key={index}>
-                    <Link href={`${process.env.URLprefix}${locales[index].short}`}>
+                    <Link href={`${process.env.URLprefix}${locales[index].short}`.replace(/\/(.*?)\/(.*?)/, '/$2')}>
                       <div className="home-language-switcher-item">
                         <span className="home-language-link">
                           {all_languages?.name}
